52c64cf16934f6f08cf9598b47e820557989f7bc
Igor borys oscar Van der smissen <igvan-de@f0r3s20.codam.nl>
2019-10-22 18:04:07 +0200

    created swap F and F'

--- main.py
+++ main.py
@@ -2,83 +2,120 @@
 
 from __future__ import print_function
 import sys
+import matplotlib.colors
 
-def	input(x):
-	if x == "F":
-		print(x)
-	if x == "B":
-		print(x)
-	if x == "R":
-		print(x)
-	if x == "L":
-		print(x)
-	if x == "U":
-		print(x)
-	if x == "D":
-		print(x)
-	if x == "F2":
-		print(x)
-	if x == "B2":
-		print(x)
-	if x == "R2":
-		print(x)
-	if x == "L2":
-		print(x)
-	if x == "U2":
-		print(x)
-	if x == "D2":
-		print(x)
-	if x == "F'":
-		print(x)
-	if x == "B'":
-		print(x)
-	if x == "R'":
-		print(x)
-	if x == "L'":
-		print(x)
-	if x == "U'":
-		print(x)
-	if x == "D'":
-		print(x)
+def	input(x, cube, n):
+	if "F" in x:
+		if "\'" in x:
+			rotate_front_l(cube, n)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			rotate_front_r(cube, n)
+	if "B" in x:
+		if "\'" in x:
+			print(x)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			print(x)
+	if "R" in x:
+		if "\'" in x:
+			print(x)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			print(x)
+	if "L" in x:
+		if "\'" in x:
+			print(x)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			print(x)
+	if "U" in x:
+		if "\'" in x:
+			print(x)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			print(x)
+	if "D" in x:
+		if "\'" in x:
+			print(x)
+		elif "2" in x:
+			print("2e", x)
+		else:
+			print(x)
 	else:
 		return
 
 def create_cube(n):
 	cube = []
-	d = 0
 	size = int(n)
+	colors = ["\033[41m   \033[0m", "\033[43m   \033[0m", "\033[44m   \033[0m",  "\033[42m   \033[0m", "\33[48;2;255;165;0m   \033[0m", "\033[47m   \033[0m"] #need to find orange ansi code!!
+	d = str(colors[0])
+	k = 0
 	for i in range(6):
+		i += 1
 		for x in range(size):
+			x += 1
 			block = []
 			for y in range(size):
+				y += 1
 				block.append(d)
-				d += 1
+				block.extend([k])
+				k += 1 #//needed to give value to blocks on each side of the cube. To swap the colors later! Or check if possible to swap by color and not by index
 			cube.append(block)
-	print_cube(cube, size, x, y)
+			if (x * y == size * size):
+				if (i == 1):
+					d = colors[1]
+				if (i == 2):
+					d = colors[2]
+				if (i == 3):
+					d = colors[3]
+				if (i == 4):
+					d = colors[4]
+				if (i == 5):
+					d = colors[5]
+	return (cube)
 
-def print_cube(cube, size, x, y):
+def print_cube(cube, size):
 	cols = len(cube)
-	rows = 0
 	if cols:
 		rows = len(cube[0])
-	var = (x + 1) * (y + 1) / size * size
 	for x in range(cols):
 		for y in range(rows):
 			print(cube[x][y], end = " ")
 		if ((x + 1) % size == 0):
 			print()
-		# print("x = ", x)
-		# print("y = ", y)
+		if ((y + 1) % size == 0):
+			print()
 		print()
-	# print("var = ", var)
+
+def	rotate_front_r(cube, n):
+	size = int(n)
+	cube[3] = cube[6]		#yellow becomes blue
+	cube[6] = cube[17]		#blue becomes white
+	cube[9] = cube[3]		#green becomes blue
+	cube[15] = cube[10]		#white becomes green
+	print_cube(cube, size)
+
+def	rotate_front_l(cube, n):
+	size = int(n)
+	cube[3] = cube[10]
+	cube[6] = cube[4]
+	cube[9] = cube[15]
+	cube[15] = cube[7]
+	print_cube(cube, size)
 
 def	main():
-	for x in sys.argv[1].split():
-		input(x)
 	if len(sys.argv) > 2:
 		n = sys.argv[2]
-		create_cube(n)
+		cube = create_cube(n)
 	else:
-		create_cube
+		cube = create_cube
+	for x in sys.argv[1].split():
+		input(x, cube, n)
 
 main()
