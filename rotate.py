a3afe406133a4f9f4b7adb3d599ecd62b6e79ec1
igvan-de <igor@DESKTOP-CB1EK1H.localdomain>
2020-05-29 13:08:06 +0200

    fixed bugg in right rotation, working on turning other way  around

--- rotate.py
+++ rotate.py
@@ -21,23 +21,25 @@ class Rotate():
 			self.sides['L'].colour[value] = left
 			value = value * 2
 
-	def turn_column_right(self, value):
+	def turn_column_right(self, value1, value2):
 		for i in range(3):
-			left = self.sides['F'].colour[value]
-			self.sides['F'].colour[value] = self.sides['D'].colour[value]
-			self.sides['D'].colour[value] = self.sides['B'].colour[value]
-			self.sides['B'].colour[value] = self.sides['U'].colour[value]
-			self.sides['U'].colour[value] = left
-			value = value * 8
+			right = self.sides['U'].colour[value1]
+			self.sides['U'].colour[value1] = self.sides['B'].colour[value2]
+			self.sides['B'].colour[value2] = self.sides['D'].colour[value1]
+			self.sides['D'].colour[value1] = self.sides['F'].colour[value1]
+			self.sides['F'].colour[value1] = right
+			value1 = value1 * 8
+			value2 = value2 * 8
 
-	def turn_column_left(self, value):
+	def turn_column_left(self, value1, value2):
 		for i in range(3):
-			left = self.sides['U'].colour[value]
-			self.sides['U'].colour[value] = self.sides['B'].colour[value]
-			self.sides['B'].colour[value] = self.sides['D'].colour[value]
-			self.sides['D'].colour[value] = self.sides['F'].colour[value]
-			self.sides['F'].colour[value] = left
-			value = value * 8
+			left = self.sides['U'].colour[value1]
+			self.sides['U'].colour[value1] = self.sides['B'].colour[value2]
+			self.sides['B'].colour[value2] = self.sides['D'].colour[value1]
+			self.sides['D'].colour[value1] = self.sides['F'].colour[value1]
+			self.sides['F'].colour[value1] = left
+			value1 = value1 * 8
+			value2 = value2 * 8
 
 	def turn_special_right_f(self):
 		temp = self.sides['R'].colour[1]
@@ -55,6 +57,40 @@ class Rotate():
 		self.sides['U'].colour[64] = temp
 		self.sides['U'].colour[128] = temp
 		self.sides['U'].colour[256] = temp
+
+	def turn_special_right_s(self):
+		temp = self.sides['R'].colour[2]
+		self.sides['R'].colour[2] = self.sides['U'].colour[8]
+		self.sides['R'].colour[16] = self.sides['U'].colour[16]
+		self.sides['R'].colour[128] = self.sides['U'].colour[32]
+		self.sides['D'].colour[8] = temp
+		self.sides['D'].colour[16] = temp
+		temp = self.sides['D'].colour[32]
+		self.sides['D'].colour[32] = self.sides['D'].colour[16]
+		self.sides['L'].colour[128] = temp
+		self.sides['L'].colour[16] = temp
+		temp = self.sides['L'].colour[2]
+		self.sides['L'].colour[2] = self.sides['L'].colour[16]
+		self.sides['U'].colour[8] = temp
+		self.sides['U'].colour[16] = temp
+		self.sides['U'].colour[32] = temp
+
+	def turn_special_right_b(self):
+		temp = self.sides['R'].colour[4]
+		self.sides['R'].colour[4] = self.sides['U'].colour[1]
+		self.sides['R'].colour[32] = self.sides['U'].colour[2]
+		self.sides['R'].colour[256] = self.sides['U'].colour[4]
+		self.sides['D'].colour[256] = temp
+		self.sides['D'].colour[128] = temp
+		temp = self.sides['D'].colour[64]
+		self.sides['D'].colour[64] = self.sides['D'].colour[128]
+		self.sides['L'].colour[64] = temp
+		self.sides['L'].colour[8] = temp
+		temp = self.sides['L'].colour[1]
+		self.sides['L'].colour[1] = self.sides['L'].colour[8]
+		self.sides['U'].colour[1] = temp
+		self.sides['U'].colour[2] = temp
+		self.sides['U'].colour[4] = temp
 
 	def rows_operations(self, operation):
 		if operation == 'U':
@@ -78,22 +114,24 @@ class Rotate():
 
 	def column_operations(self, operation):
 		if operation == 'L':
-			value = 1
-			self.turn_column_right(value)
-		if operation == 'L\'':
-			value = 1
-			self.turn_column_left(value)
-		if operation == 'M':
-			value = 2
-			self.turn_column_right(value)
-		if operation == 'M\'':
-			value = 2
-			self.turn_column_left(value)
+			value1 = 1
+			value2 = 4
+			self.turn_column_left(value1, value2)
+		# if operation == 'L\'':
+		# 	value = 1
+		# 	self.turn_column_left(value)
+		# if operation == 'M':
+		# 	value = 2
+		# 	self.turn_column_right(value)
+		# if operation == 'M\'':
+		# 	value = 2
+		# 	self.turn_column_left(value)
 		if operation == 'R':
-			value = 4
-			self.turn_column_right(value)
-		if operation == 'R\'':
-			self.turn_column_right()
+			value1 = 4
+			value2 = 1
+			self.turn_column_right(value1, value2)
+		# if operation == 'R\'':
+		# 	self.turn_column_right()
 
 	def special_operations(self, operation):
 		if operation == 'F':
@@ -101,7 +139,7 @@ class Rotate():
 			self.turn_special_right_f()
 		if operation == 'S':
 			value = 8
-			self.turn_special_right_S()
+			self.turn_special_right_s()
 		if operation == 'B':
 			value = 1
-			self.turn_special_right_B()
+			self.turn_special_right_b()
